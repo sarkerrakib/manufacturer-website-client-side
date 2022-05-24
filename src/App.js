51b2edc3805f88e5551b57c9b456fc285bdd9d27
sarkerrakib <freelancerrakib25@gmail.com>
2022-05-24 13:01:03 +0600

    notfound page done

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import { Routes, Route, Link } from "react-router-dom";
 import Home from './Pages/Home/Home';
 import MyPortfolio from './Pages/Portfolio/MyPortfolio';
 import Login from './Pages/Login/Login';
+import NotFound from './Pages/Shared/NotFound';
 
 function App() {
   return (
@@ -13,6 +14,7 @@ function App() {
         <Route path="/" element={<Home />} />
         <Route path="portfolio" element={<MyPortfolio />} />
         <Route path="login" element={<Login />}></Route>
+        <Route path='*' element={<NotFound></NotFound>}></Route>
       </Routes>
     </div>
   );
---
+++ src/Pages/Shared/NotFound.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import notFound from '../../images/notFound.png';
+
+const NotFound = () => {
+    return (
+
+        <div className=''>
+            <img className='object-cover h-200 w-200' src={notFound} alt="" />
+        </div>
+
+    );
+};
+
+export default NotFound;
