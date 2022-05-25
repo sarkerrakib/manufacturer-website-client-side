285f51ee1fdd64dddcd5a90dc9ef3ef42b946d98
sarkerrakib <freelancerrakib25@gmail.com>
2022-05-26 00:19:18 +0600

    log in form

--- src/Pages/Login/Login.js
+++ src/Pages/Login/Login.js
@@ -1,52 +1,100 @@
 import React from 'react';
+import auth from '../../firebase.init';
+import { useSignInWithGoogle } from 'react-firebase-hooks/auth';
+import { useForm } from 'react-hook-form';
 
 const Login = () => {
+    const [signInWithGoogle, user, loading, error] = useSignInWithGoogle(auth);
+    const { register, formState: { errors }, handleSubmit } = useForm();
+
+    if (user) {
+        console.log(user);
+    }
+
+    const onSubmit = data => {
+        console.log(data)
+    };
+
     return (
-        <section class="h-screen px-14">
-            <div class="container px-6 py-12 h-full">
-                <div class="flex justify-center items-center flex-wrap h-full g-6 text-gray-800">
-                    <div class="md:w-8/12 lg:w-6/12 mb-12 md:mb-0">
+        <section className="h-screen px-14 ">
+            <div className="container px-6 py-12 h-full">
+                <div className="flex justify-center items-center flex-wrap h-full g-6 text-gray-800">
+                    <div className="md:w-8/12 lg:w-6/12 mb-12 md:mb-0">
                         <img
                             src="https://mdbcdn.b-cdn.net/img/Photos/new-templates/bootstrap-login-form/draw2.svg"
-                            class="w-full"
+                            className="w-full"
                             alt="Phone "
                         />
                     </div>
-                    <div class="md:w-8/12 lg:w-5/12 lg:ml-20">
-                        <form>
+                    <div className="md:w-8/12 lg:w-5/12 lg:ml-20 ">
+                        <form onSubmit={handleSubmit(onSubmit)}>
+                            
+
                             {/* <!-- Email input --> */}
-                            <div class="mb-6">
+                            <div className="mb-6">
                                 <input
-                                    type="text"
-                                    class="form-control block w-full px-4 py-2 text-xl font-normal text-gray-700 bg-white bg-clip-padding border border-solid border-gray-300 rounded transition ease-in-out m-0 focus:text-gray-700 focus:bg-white focus:border-blue-600 focus:outline-none"
+                                    type="email"
+                                    className="form-control block w-full px-4 py-2 text-xl font-normal text-gray-700 bg-white bg-clip-padding border border-solid border-gray-300 rounded transition ease-in-out m-0 focus:text-gray-700 focus:bg-white focus:border-blue-600 focus:outline-none"
                                     placeholder="Email address"
+                                    {...register("email", {
+                                        required: {
+                                            value: true,
+                                            message: 'Email is Required'
+                                        },
+                                        pattern: {
+                                          value: /[a-z0-9]+@[a-z]+\.[a-z]{2,3}/,
+                                          message: 'Provide a valid Email' 
+                                        }
+                                      })}
                                 />
+                                <label className='label'>
+                                {errors.firstName?.type === 'required' && <span className='label-text-alt text-red-500'>{errors.email.message}</span>}
+                                {errors.firstName?.type === 'pattern' && <span className='label-text-alt text-red-500'>{errors.email.message}</span>}
+                                      
+                                </label>
                             </div>
 
                             {/* <!-- Password input --> */}
-                            <div class="mb-6">
+                            <div className="mb-6">
                                 <input
                                     type="password"
-                                    class="form-control block w-full px-4 py-2 text-xl font-normal text-gray-700 bg-white bg-clip-padding border border-solid border-gray-300 rounded transition ease-in-out m-0 focus:text-gray-700 focus:bg-white focus:border-blue-600 focus:outline-none"
+                                    className="form-control block w-full px-4 py-2 text-xl font-normal text-gray-700 bg-white bg-clip-padding border border-solid border-gray-300 rounded transition ease-in-out m-0 focus:text-gray-700 focus:bg-white focus:border-blue-600 focus:outline-none"
                                     placeholder="Password"
+                                    {...register("password", {
+                                        required: {
+                                            value: true,
+                                            message: 'Password is Required'
+                                        },
+                                        minLength: {
+                                          value: 6,
+                                          message: 'Must be 6 Characters or longer' 
+                                        }
+                                      })}
                                 />
+                                <label className='label'>
+                                {errors.password?.type === 'required' && <span className='label-text-alt text-red-500'>{errors.email.message}</span>}
+                                {errors.password?.type === 'minLength' && <span className='label-text-alt text-red-500'>{errors.email.message}</span>}
+                                      
+                                </label>
                             </div>
 
-                            <div class="flex justify-between items-center mb-6">
-                                <div class="form-group form-check">
+
+                            {/* //labels */}
+                            <div className="flex justify-between items-center mb-6">
+                                <div className="form-group form-check">
                                     <input
                                         type="checkbox"
-                                        class="form-check-input appearance-none h-4 w-4 border border-gray-300 rounded-sm bg-white checked:bg-blue-600 checked:border-blue-600 focus:outline-none transition duration-200 mt-1 align-top bg-no-repeat bg-center bg-contain float-left mr-2 cursor-pointer"
+                                        className="form-check-input appearance-none h-4 w-4 border border-gray-300 rounded-sm bg-white checked:bg-blue-600 checked:border-blue-600 focus:outline-none transition duration-200 mt-1 align-top bg-no-repeat bg-center bg-contain float-left mr-2 cursor-pointer"
                                         id="exampleCheck3"
                                         checked
                                     />
-                                    <label class="form-check-label inline-block text-gray-800" for="exampleCheck2"
+                                    <label className="form-check-label inline-block text-gray-800" for="exampleCheck2"
                                     >Remember me</label
                                     >
                                 </div>
                                 <a
                                     href="#!"
-                                    class="text-blue-600 hover:text-blue-700 focus:text-blue-700 active:text-blue-800 duration-200 transition ease-in-out"
+                                    className="text-blue-600 hover:text-blue-700 focus:text-blue-700 active:text-blue-800 duration-200 transition ease-in-out"
                                 >Forgot password?</a
                                 >
                             </div>
@@ -54,59 +102,66 @@ const Login = () => {
                             {/* <!-- Submit button --> */}
                             <button
                                 type="submit"
-                                class="inline-block px-7 py-3 bg-blue-600 text-white font-medium text-sm leading-snug uppercase rounded shadow-md hover:bg-blue-700 hover:shadow-lg focus:bg-blue-700 focus:shadow-lg focus:outline-none focus:ring-0 active:bg-blue-800 active:shadow-lg transition duration-150 ease-in-out w-full"
+                                className="inline-block px-7 py-3 bg-blue-600 text-white font-medium text-sm leading-snug uppercase rounded shadow-md hover:bg-blue-700 hover:shadow-lg focus:bg-blue-700 focus:shadow-lg focus:outline-none focus:ring-0 active:bg-blue-800 active:shadow-lg transition duration-150 ease-in-out w-full"
                                 data-mdb-ripple="true"
                                 data-mdb-ripple-color="light"
                             >
-                                Sign in
+                                Log In
                             </button>
 
                             <div
-                                class="flex items-center my-4 before:flex-1 before:border-t before:border-gray-300 before:mt-0.5 after:flex-1 after:border-t after:border-gray-300 after:mt-0.5"
+                                className="flex items-center my-4 before:flex-1 before:border-t before:border-gray-300 before:mt-0.5 after:flex-1 after:border-t after:border-gray-300 after:mt-0.5"
                             >
-                                <p class="text-center font-semibold mx-4 mb-0">OR</p>
+                                <p className="text-center font-semibold mx-4 mb-0">OR</p>
                             </div>
 
-                            <a
-                                class="bg-[#3b5998] px-7 py-3 text-white font-medium text-sm leading-snug uppercase rounded shadow-md hover:shadow-lg focus:shadow-lg focus:outline-none focus:ring-0 active:shadow-lg transition duration-150 ease-in-out w-full flex justify-center items-center mb-3"
-                                // style="background-color: #3b5998"
-                                href="#!"
-                                role="button"
-                                data-mdb-ripple="true"
-                                data-mdb-ripple-color="light"
+                            <button
+                                onClick={() => signInWithGoogle()}
+                                className=' w-full'
                             >
-                                {/* <!-- Facebook --> */}
-                                <svg
-                                    xmlns="http://www.w3.org/2000/svg"
-                                    viewBox="0 0 320 512"
-                                    class="w-3.5 h-3.5 mr-2"
+                                <a
+                                    className="bg-primary px-7 py-3 text-white font-medium text-sm leading-snug uppercase rounded shadow-md hover:shadow-lg focus:shadow-lg focus:outline-none focus:ring-0 active:shadow-lg transition duration-150 ease-in-out w-full flex justify-center items-center mb-3"
+                                    // style="background-color: #3b5998"
+                                    href="#!"
+                                    role="button"
+                                    data-mdb-ripple="true"
+                                    data-mdb-ripple-color="light"
                                 >
-                                    {/* <!--! Font Awesome Pro 6.0.0 by @fontawesome - https://fontawesome.com License - https://fontawesome.com/license (Commercial License) Copyright 2022 Fonticons, Inc. --> */}
-                                    <path
-                                        fill="currentColor"
-                                        d="M279.14 288l14.22-92.66h-88.91v-60.13c0-25.35 12.42-50.06 52.24-50.06h40.42V6.26S260.43 0 225.36 0c-73.22 0-121.08 44.38-121.08 124.72v70.62H22.89V288h81.39v224h100.17V288z"
-                                    /></svg>Continue with Facebook
-                            </a>
-                            <a
-                                class="bg-[#55acee] px-7 py-3 text-white font-medium text-sm leading-snug uppercase rounded shadow-md hover:shadow-lg focus:shadow-lg focus:outline-none focus:ring-0 active:shadow-lg transition duration-150 ease-in-out w-full flex justify-center items-center"
+                                    {/* <!-- Facebook --> */}
+                                    <svg
+                                        xmlns="http://www.w3.org/2000/svg"
+                                        viewBox="0 0 48 48"
+                                        className="w-3.5 h-3.5 mr-2"
+                                    >
+                                        {/* <!--! Font Awesome Pro 6.0.0 by @fontawesome - https://fontawesome.com License - https://fontawesome.com/license (Commercial License) Copyright 2022 Fonticons, Inc. --> */}
+                                        <path
+                                            fill="currentColor"
+                                            d="M44.5 20H24v8.5h11.8C34.7 33.9 30.1 37 24 37c-7.2 0-13-5.8-13-13s5.8-13 13-13c3.1 0 5.9 1.1 8.1 2.9l6.4-6.4C34.6 4.1 29.6 2 24 2 11.8 2 2 11.8 2 24s9.8 22 22 22c11 0 21-8 21-22 0-1.3-.2-2.7-.5-4z"
+                                        /></svg>Continue with Google
+                                </a>
+                            </button>
+
+                            {/* <!-- Twitter --> */}
+                            {/* <a
+                                class="bg-primary px-7 py-3 text-white font-medium text-sm leading-snug uppercase rounded shadow-md hover:shadow-lg focus:shadow-lg focus:outline-none focus:ring-0 active:shadow-lg transition duration-150 ease-in-out w-full flex justify-center items-center"
                                 // style={"background-color: #55acee"}
                                 href="#!"
                                 role="button"
                                 data-mdb-ripple="true"
                                 data-mdb-ripple-color="light"
                             >
-                                {/* <!-- Twitter --> */}
+                                
                                 <svg
                                     xmlns="http://www.w3.org/2000/svg"
                                     viewBox="0 0 512 512"
                                     class="w-3.5 h-3.5 mr-2"
                                 >
-                                    {/* <!--! Font Awesome Pro 6.0.0 by @fontawesome - https://fontawesome.com License - https://fontawesome.com/license (Commercial License) Copyright 2022 Fonticons, Inc. --> */}
+                                    <!--! Font Awesome Pro 6.0.0 by @fontawesome - https://fontawesome.com License - https://fontawesome.com/license (Commercial License) Copyright 2022 Fonticons, Inc. -->
                                     <path
                                         fill="currentColor"
                                         d="M459.37 151.716c.325 4.548.325 9.097.325 13.645 0 138.72-105.583 298.558-298.558 298.558-59.452 0-114.68-17.219-161.137-47.106 8.447.974 16.568 1.299 25.34 1.299 49.055 0 94.213-16.568 130.274-44.832-46.132-.975-84.792-31.188-98.112-72.772 6.498.974 12.995 1.624 19.818 1.624 9.421 0 18.843-1.3 27.614-3.573-48.081-9.747-84.143-51.98-84.143-102.985v-1.299c13.969 7.797 30.214 12.67 47.431 13.319-28.264-18.843-46.781-51.005-46.781-87.391 0-19.492 5.197-37.36 14.294-52.954 51.655 63.675 129.3 105.258 216.365 109.807-1.624-7.797-2.599-15.918-2.599-24.04 0-57.828 46.782-104.934 104.934-104.934 30.213 0 57.502 12.67 76.67 33.137 23.715-4.548 46.456-13.32 66.599-25.34-7.798 24.366-24.366 44.833-46.132 57.827 21.117-2.273 41.584-8.122 60.426-16.243-14.292 20.791-32.161 39.308-52.628 54.253z"
                                     /></svg>Continue with Twitter
-                            </a>
+                            </a> */}
                         </form>
                     </div>
                 </div>
