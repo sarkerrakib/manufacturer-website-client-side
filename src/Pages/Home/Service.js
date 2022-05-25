994166ad54cd184f2824c5e28d3f6c8dbd3f9ac3
sarkerrakib <freelancerrakib25@gmail.com>
2022-05-25 19:56:20 +0600

    store updated

--- src/Pages/Home/Service.js
+++ src/Pages/Home/Service.js
@@ -1,20 +1,27 @@
 import React from 'react';
+import { useNavigate } from 'react-router-dom';
 
 const Service = ({service}) => {
+    const {_id, name, img, description, price, order} = service;
+    const navigate = useNavigate();
+
+    const navigateToServiceDetail = id =>{
+        navigate(`/service/${id}`);
+    }
     
 
     return (
         <div class="card lg:max-w-lg bg-base-100 shadow-xl px-5">
             <figure class="px-8 pt-5">
-                <img src={service.img} alt="Shoes" class="rounded-xl" />
+                <img src={img} alt="Shoes" class="rounded-xl" />
             </figure>
             <div class="card-body items-center text-center">
-                <h2 class="card-title">{service.name}</h2>
-                <p>{service.description}</p>
-                <p>Min-Order: {service.order}</p>
-                <p>Unit-Price: {service.price}</p>
+                <h2 class="card-title">{name}</h2>
+                <p>{description}</p>
+                <p>Min-Order: {order}</p>
+                <p>Unit-Price: {price}</p>
                 <div class="card-actions">
-                    <button class="btn btn-primary text-white">Buy Now</button>
+                    <button onClick={() => navigateToServiceDetail(_id)} class="btn btn-primary text-white">Buy Now</button>
                 </div>
             </div>
         </div>
--- src/Pages/Home/Services.js
+++ src/Pages/Home/Services.js
@@ -1,69 +1,14 @@
-import React from 'react';
-import da1 from '../../images/da1.png';
-import da2 from '../../images/da2.png';
-import da3 from '../../images/da3.png';
-import da4 from '../../images/da4.png';
-import da5 from '../../images/da5.png';
-import da6 from '../../images/da6.png';
+import React, { useEffect, useState } from 'react';
 import Service from './Service';
 
 const Services = () => {
-    const services = [
-        {
-            _id: 1,
-            name: 'da camera 1',
-            description: '8MP Smart Dual Illumination Active Deterrence Vari-focal Bullet WizSense Network Camera. Outputs max. 8 MP (3840 × 2160)@25/30 fps. H.265 codec, high compression rate, ultra-low bit rate.Built-in warm light and IR LED; the max. IR illumination distance is 50 m and warm light distance is 40 m.',
-            img: da1,
-            order: '100',
-            price: '120'
+    const [services, setServices] = useState([]);
 
-        },
-        {
-            _id: 2,
-            name: 'da camera 1',
-            description: '8MP Smart Dual Illumination Active Deterrence Vari-focal Bullet WizSense Network Camera. Outputs max. 8 MP (3840 × 2160)@25/30 fps. H.265 codec, high compression rate, ultra-low bit rate.Built-in warm light and IR LED; the max. IR illumination distance is 50 m and warm light distance is 40 m.',
-            img: da2,
-            order: '100',
-            price: '120'
-
-        },
-        {
-            _id: 3,
-            name: 'da camera 1',
-            description: '8MP Smart Dual Illumination Active Deterrence Vari-focal Bullet WizSense Network Camera. Outputs max. 8 MP (3840 × 2160)@25/30 fps. H.265 codec, high compression rate, ultra-low bit rate.Built-in warm light and IR LED; the max. IR illumination distance is 50 m and warm light distance is 40 m.',
-            img: da3,
-            order: '100',
-            price: '120'
-
-        },
-        {
-            _id: 4,
-            name: 'da camera 1',
-            description: '8MP Smart Dual Illumination Active Deterrence Vari-focal Bullet WizSense Network Camera. Outputs max. 8 MP (3840 × 2160)@25/30 fps. H.265 codec, high compression rate, ultra-low bit rate.Built-in warm light and IR LED; the max. IR illumination distance is 50 m and warm light distance is 40 m.',
-            img: da4,
-            order: '100',
-            price: '120'
-
-        },
-        {
-            _id: 5,
-            name: 'da camera 1',
-            description: '8MP Smart Dual Illumination Active Deterrence Vari-focal Bullet WizSense Network Camera. Outputs max. 8 MP (3840 × 2160)@25/30 fps. H.265 codec, high compression rate, ultra-low bit rate.Built-in warm light and IR LED; the max. IR illumination distance is 50 m and warm light distance is 40 m.',
-            img: da5,
-            order: '100',
-            price: '120'
-
-        },
-        {
-            _id: 6,
-            name: 'da camera 1',
-            description: '8MP Smart Dual Illumination Active Deterrence Vari-focal Bullet WizSense Network Camera. Outputs max. 8 MP (3840 × 2160)@25/30 fps. H.265 codec, high compression rate, ultra-low bit rate.Built-in warm light and IR LED; the max. IR illumination distance is 50 m and warm light distance is 40 m.',
-            img: da6,
-            order: '100',
-            price: '120'
-
-        },
-    ];
+    useEffect(() => {
+        fetch('http://localhost:5000/store')
+            .then(res => res.json())
+            .then(data => setServices(data));
+    }, [])
 
     return (
         <div className='my-28 px-12'>
